e949b243e6ec03ef928486c1bdc760cf96a38ee4
SupratikDey <spratik.dey@gmail.com>
2026-04-02 10:27:36 +0530

    UC1

---
+++ App/src/BookMyStay.java
@@ -0,0 +1,24 @@
+/**
+ * Book My Stay App
+ *
+ * A simple Hotel Booking Management System demonstrating
+ * the entry point of a Java application.
+ *
+ * @author SPD
+ * @version 1.0
+ */
+public class BookMyStay {
+
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("=== Book My Stay App ===");
+
+        // Application details
+        System.out.println("Hotel Booking Management System v1.0");
+
+        // Additional message
+        System.out.println("Welcome! Your stay booking experience begins here.");
+
+    }
+}
